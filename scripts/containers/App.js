9f9ab15bd53eeb6d3b19ea98f5fa47315a435837
adityasan92 <adityasan92@gmail.com>
2015-12-21 22:22:07 -0500

    Added a store as well as a container

---
+++ scripts/containers/App.js
@@ -0,0 +1,12 @@
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+
+class App extends Component {
+
+  render(){
+    
+  }
+}
+
+
+export default connect()(App);

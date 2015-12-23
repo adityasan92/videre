f66b1296276154904d89a96046e3b61cff551262
adityasan92 <adityasan92@gmail.com>
2015-12-23 01:16:56 -0500

    Added the environment setup part

--- scripts/containers/App.js
+++ scripts/containers/App.js
@@ -1,12 +1,40 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {initEnvironment} from '../actions/environment';
 
 class App extends Component {
 
+  componentDidMount () {
+       const {dispatch} = this.props;
+       console.log("Init the env");
+       dispatch(initEnvironment());
+
+   }
+
   render(){
-    
+    console.log(this.props);
+    return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
   }
 }
 
+App.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    path: PropTypes.array.isRequired
+};
 
-export default connect()(App);
+function mapStateToProps(state) {
+    const {environment, navigator} = state;
+    console.log(environment);
+    return {
+        height: environment.height,
+        isMobile: environment.isMobile,
+        //path: navigator.route.path,
+        width: environment.width
+    };
+}
+
+export default connect(mapStateToProps)(App);
--- scripts/reducers/index.js
+++ scripts/reducers/index.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux';
+import environment from '../reducers/environment';
 
 const rootReducer = combineReducers({
-
+  environment
 });
 
 export default rootReducer;

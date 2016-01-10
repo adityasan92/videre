1d42c272485625c50c038b7baed04db1abc9652a
adityasan92 <adityasan92@gmail.com>
2016-01-09 22:19:14 -0500

    Added videre constants and compoenents for videre and Videres

---
+++ scripts/constants/VidereConstants.js
@@ -0,0 +1,31 @@
+export const CHANGE_TYPES = {
+    NEXT: 'next',
+    PLAY: 'play',
+    SHUFFLE: 'shuffle'
+};
+
+export const GENRES = [
+    'chill',
+    'deep',
+    'dubstep',
+    'house',
+    'progressive',
+    'tech',
+    'trance',
+    'tropical'
+];
+
+export const GENRES_MAP = (function(){
+    let result = {};
+    GENRES.forEach(function(genre){
+        result[genre] = 1;
+    });
+
+    return result;
+})();
+
+
+export const IMAGE_SIZES = {
+    LARGE: 't300x300',
+    XLARGE: 't500x500'
+};

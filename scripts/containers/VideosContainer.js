0950498c0b41ab2924ef952b9303d6582a0f6366
adityasan92 <adityasan92@gmail.com>
2016-01-13 23:20:08 -0500

    enabled dragablity

--- scripts/containers/VideosContainer.js
+++ scripts/containers/VideosContainer.js
@@ -1,11 +1,14 @@
 import React, {Component, PropTypes} from 'react';
-
+//var Draggabilly = require('draggabilly');
+var $draggable = $('.draggable').draggabilly({
+  // options...
+})
 class VideosContainer extends Component {
     render(){
       const {dispatch} = this.props;
       return (
         <div className="row">
-          <div className="col-md-4">.col-md-1</div>
+          <div className="col-md-4 draggable">.col-md-1</div>
           <div className="col-md-4">.col-md-1</div>
           <div className="col-md-4">.col-md-1</div>
         </div>

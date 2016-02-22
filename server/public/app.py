5469157ef1edb70d2dbba82be1baeed84d753f1e
adityasan92 <adityasan92@gmail.com>
2016-02-21 19:29:35 -0500

    cleaned stuff up

--- server/public/app.py
+++ server/public/app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template
 
 import os
 temp_folder = os.path.abspath(__file__)
-app = Flask(__name__)
+app = Flask(__name__,  static_url_path='')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
